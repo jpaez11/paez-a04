e9af1cf2f47771076e82bb398fcb6d9438da7b8c
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-16 18:45:07 -0400

    solution code for ex42

---
+++ exercise43/src/main/java/baseline/solution43.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jorge Paez
+ *
+ */
+
+package baseline;
+
+public class solution43 {
+    /*
+    Pseudocode:
+    websites are pretty cool, let's try and write one
+    but this one will prompt the user to:
+    give a name for the site
+    ask who the author is
+    ask user if they want a folder for either JavaScript or CSS files
+    and generate an index.html that contains the name of the site
+    inside the <title> tag and the author in a <meta> tag
+     */
+}
