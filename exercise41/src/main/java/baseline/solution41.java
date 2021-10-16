a570b2e6469124536cb10aab74f1ba5101684c3b
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-15 23:16:43 -0400

    first, the pseudocode

---
+++ exercise41/src/main/java/baseline/solution41.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jorge Paez
+ */
+package baseline;
+
+public class solution41 {
+    /*
+    Pseudocode:
+    using a text file, the program will read a list of names
+    and will also sort them alphabetically
+     */
+}
