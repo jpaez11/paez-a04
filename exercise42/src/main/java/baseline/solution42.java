6541ae1ebaf7cf50419a11e103be852571e40a85
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-16 17:18:24 -0400

    pseudoode (ex42)

---
+++ exercise42/src/main/java/baseline/solution42.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jorge Paez
+ *
+
+ */
+package baseline;
+
+public class solution42 {
+    /*
+    Pseudocode:
+    making a program that parses in a data file, the records will be processed
+    and formatted as a table.
+
+      */
+}
