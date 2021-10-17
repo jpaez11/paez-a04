5ea6d9ed240da29e41072e02b48238ce5536bc98
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-17 14:08:52 -0400

    pseudocode for ex44

---
+++ ex44/src/main/java/baseline/solution44.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class solution44 {
+    /*
+    Pseudocode:
+    this program will take a product name that retrieves the current
+    price in a data file in JSON. essentially, it will be parsed thru
+    JSON.
+     */
+}
