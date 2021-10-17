718e75d0c0a82fb977b5ca50c36563af93b284c1
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-17 17:08:24 -0400

    pseudocode for ex45

--- ex44/src/main/java/baseline/solution44.java
+++ ex44/src/main/java/baseline/solution44.java
@@ -1,10 +1,21 @@
+/*
+
+UCF COP3330 Fall 2021 Assignment 4 Solutions
+Copyright 2021 Jorge Paez
+
+   Pseudocode:
+   this program will take a product name that retrieves the current
+   price in a data file in JSON. essentially, it will be parsed thru
+   JSON.
+    */
+
 package baseline;
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 public class solution44 {
-    /*
-    Pseudocode:
-    this program will take a product name that retrieves the current
-    price in a data file in JSON. essentially, it will be parsed thru
-    JSON.
-     */
+
+
 }
---
+++ untitled/src/main/java/baseline/solution45.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021
+ *
+
+ */
+package baseline;
+import java.io.*;
+
+public class solution45 {
+    /*
+    Psuedocode:
+    reading a  text file, that will soon be modified via using a word to
+    find the said word. ex: using 'utilize' to replace 'use.'
+     */
+
+}
