45673c09412f6127e3f8f5a93f14942ecb8caa95
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-16 22:26:21 -0400

    solution for ex43

--- exercise43/src/main/java/baseline/solution43.java
+++ exercise43/src/main/java/baseline/solution43.java
@@ -1,14 +1,8 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 4 Solutions
- *  Copyright 2021 Jorge Paez
+ *  Copyright 2021
  *
- */
-
-package baseline;
-
-public class solution43 {
-    /*
-    Pseudocode:
+ *     Pseudocode:
     websites are pretty cool, let's try and write one
     but this one will prompt the user to:
     give a name for the site
@@ -16,5 +10,82 @@ public class solution43 {
     ask user if they want a folder for either JavaScript or CSS files
     and generate an index.html that contains the name of the site
     inside the <title> tag and the author in a <meta> tag
-     */
-}
+ */
+
+package baseline;
+import java.io.*;
+import java.text.MessageFormat;
+import java.util.*;
+
+public class solution43 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in); //user inputs :D
+
+        //initiating strings for the prompts
+        String author = null;
+        String nameOfSite;
+        String path;
+
+        char javaScript= 'N'; //for the javaScript files
+        char CSS = 'N'; //this is for the CSS files
+
+        System.out.print("What's the name of the Site? \n");
+        nameOfSite= scanner.nextLine();
+
+        System.out.print("Folder for JavaScript?");
+        javaScript = scanner.next().charAt(0); //making the folder if selected
+
+        System.out.print("Folder for CSS?");
+        CSS = scanner.next().charAt(0); //same, but CSS
+
+        path = System.getProperty("user.directory"); //let's make a directory for the user
+        path +=  "//" +nameOfSite; //here's to get the desired site name
+
+        File site = new File(path); //initiating files for the website
+
+        boolean createWebsite = site.mkdir(); //website's directory xd
+
+        //website itself
+        if (createWebsite) {
+
+            try {
+                //baby's first HTML
+                FileWriter skeleton = new FileWriter(new File(path + "\\index.html")); //html pathing
+                skeleton.write("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n");
+                skeleton.write(MessageFormat.format("\t<meta author=\"{0}\">\n", author));
+                skeleton.write("\t<title>" + nameOfSite + "</title>\n");
+                skeleton.write("</head>\n<body>\n\n</body>\n</html>");
+                skeleton.close();
+                System.out.println("Created" + path + "\\index.html");
+
+                File javascriptFolder = new File(path + "\\js");
+                boolean javascriptCreated = javascriptFolder.mkdir();
+                if (javascriptCreated) {
+                    System.out.println("Created " + javascriptFolder + "\\");
+                } else {
+                    System.out.println("JavaScript folder not created!!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+            }
+            if(CSS == 'y' || CSS == 'Y')
+            {
+                File cssFolder = new File(path+"\\css");
+                boolean cssCreated = cssFolder.mkdir();
+                if(cssCreated)
+                {
+                    System.out.println("Created " + cssFolder+"\\");
+                }
+                else
+                {
+                    System.out.println("CSS folder not created!!");
+                }
+            }
+        }
+
+            }
+        }
+
+
