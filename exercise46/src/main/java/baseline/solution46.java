6f12c25eb7f64c885fd6ef53d6a7e76a548a2737
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-17 19:25:20 -0400

    pseudocode for ex46

---
+++ exercise46/src/main/java/baseline/solution46.java
@@ -0,0 +1,11 @@
+package baseline;
+import java.io.*;
+
+public class solution46 {
+    /*
+    Pseudocode:
+    using an array, this program will count the amount of times
+    a word is used in a file. and then will prompt the amount of times
+    the word was used via histogram.
+     */
+}
