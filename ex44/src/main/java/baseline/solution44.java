a96ce6036fec7315e0575431652fb5482cea64b7
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-17 20:15:40 -0400

    please help me solve exercise44, i am stuck and almost pulled my hair out :') xd

--- ex44/src/main/java/baseline/solution44.java
+++ ex44/src/main/java/baseline/solution44.java
@@ -7,6 +7,9 @@ Copyright 2021 Jorge Paez
    this program will take a product name that retrieves the current
    price in a data file in JSON. essentially, it will be parsed thru
    JSON.
+
+   note:
+   (idk why the JSON isn't being read, please help)
     */
 
 package baseline;
@@ -16,6 +19,44 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class solution44 {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser(); //creating a JSON parser
+        try {
+            //to read a file, a parser will be used and stored in an object
+            Object ob = parser.parse(new FileReader("D:/product.json"));
+            JSONObject jsonObject = (JSONObject) ob;
+            JSONArray subj = (JSONArray) jsonObject.get("products"); //making an array to read the products from file
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in)); //user inputs
+
+            String in;
+            int i = 0;
+            System.out.print("What's the name of the product?");
+            in = bufferedReader.readLine(); //reading inputs
+
+            //creating iterations for the array
+            Iterator it = subj.it();
+
+            //making a loop for the iterator
+            while (it.hasNext()) {
+                JSONObject jSON = (JSONObject) it.next(); //another JSON
+
+                //getting the value for the name
+                String names = (String) jSON.get("names");
+
+                //time to compare with the given
+                if (in.toLowerCase().equals(names.toLowerCase())) {
+                    System.out.println("Name: " + names);
+                    System.out.println("Price: " + jSON.get("price"));
+                    System.out.println("Quantity: " + jSON.get("quantity"));
+                    i = 1;
+                }
+                if (i == 0) { //if value is zero, then it isn't found
+                    System.out.println("Product not found, please try again.");
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
 
 
-}
+            }
+        }
--- exercise46/src/main/java/baseline/solution46.java
+++ exercise46/src/main/java/baseline/solution46.java
@@ -1,11 +1,50 @@
-package baseline;
-import java.io.*;
-
-public class solution46 {
-    /*
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021
+ *
+ *
     Pseudocode:
     using an array, this program will count the amount of times
     a word is used in a file. and then will prompt the amount of times
     the word was used via histogram.
-     */
+*
+*/
+
+package baseline;
+import java.io.*;
+
+public class solution46 {
+    public static void main(String[] args) throws IOException{
+        File file1=new File("exercise46_input.txt"); //creating the text file
+        String[] words=null; //array for the words
+        //objects for the file, and reading of
+        FileReader fr = new FileReader(file1);
+        BufferedReader br = new BufferedReader(fr);
+
+        String s;
+
+        String in="badger"; //searching
+        int count=0; //it starts at 0
+
+            //starting a while loop to read content from the file
+        while((s=br.readLine())!=null)
+        {
+            words=s.split(" "); //making it look cleaner
+            for (String word : words)
+
+            {
+                if (word.equals(in)){ //time to actually search
+                    count++; //if the word shows up +1
+                }
+
+            }
+
+        }
+        if(count!=0){ //to make sure it isn't zero
+            System.out.println("Here's how many times the word: "+count+ " was used. ");
+        } else {
+            System.out.println("The typed word is not in the file!");
+        }
+        fr.close(); //avoiding any mem leak
+    }
 }
